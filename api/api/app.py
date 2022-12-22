f0c4e9b9380d506bc7a2118c8017ea7cca639d72
Asim Jawahir <asimjawahir123@gmail.com>
2022-12-22 23:16:24 +0000

    Updated API Server

--- api/api/app.py
+++ api/api/app.py
@@ -2,11 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_api import status
 import os
+from flask_cors import CORS
 
 DB_NAME = 'cohereQA'
 
 app = Flask(__name__, static_folder='static')
-
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['UPLOAD_FOLDER'] = os.path.join('static', 'images')
@@ -16,15 +17,15 @@ db = SQLAlchemy(app)
 
 @app.before_first_request
 def create_tables():
-    db.create_all()
+  db.create_all()
 
 
 @app.route('/')
 def home():
-    return "Welcome to cohereQA", status.HTTP_200_OK
+  return "Welcome to cohereQA", status.HTTP_200_OK
 
 
 from views import *
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+  app.run(host='0.0.0.0')
